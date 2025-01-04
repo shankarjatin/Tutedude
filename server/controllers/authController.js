9ee8cb864d4fb45c719f3edaa8f531cac4491572
shankarjatin <shankarjatin1005@gmail.com>
2025-01-05 00:31:38 +0530

    user login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,15 +1,30 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
 exports.register = async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, interests } = req.body; // Include interests in request body
     try {
       const hashedPassword = await bcrypt.hash(password, 10);
-      const user = new User({ username, password: hashedPassword });
+      const user = new User({ username, password: hashedPassword, interests });
       await user.save();
       res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
       res.status(400).json({ error: 'Registration failed' });
+    }
+  };
+  
+  exports.login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ username });
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+      res.json({ token });
+    } catch (error) {
+      res.status(500).json({ error: 'Login failed' });
     }
   };
