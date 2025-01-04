ad804135698b4848592e0151677d035654d2a549
shankarjatin <shankarjatin1005@gmail.com>
2025-01-05 00:27:43 +0530

    register controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ username, password: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      res.status(400).json({ error: 'Registration failed' });
+    }
+  };
