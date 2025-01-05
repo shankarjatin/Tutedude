4bc575fa4c8d3555562c7fc8a715e1f79528b8ed
shankarjatin <shankarjatin1005@gmail.com>
2025-01-05 19:02:50 +0530

    vercel config

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 // src/constants.js
 
 // Define the base URL for the backend API
-// export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://tutedude-x2vx.onrender.com";
+export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "https://tutedude-x2vx.onrender.com";
