834b7d29c13beb7da0344cebb23565a782cd2902
shankarjatin <shankarjatin1005@gmail.com>
2025-01-05 07:53:24 +0530

    server change

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     try {
       console.log('Pinging server to keep it awake...');
       // Change this to your actual server's public URL
-      await axios.get('http://localhost:5000');
+      await axios.get('https://tutedude-x2vx.onrender.com');
       console.log('Server pinged successfully');
     } catch (error) {
       console.error('Error pinging the server:', error.message);
